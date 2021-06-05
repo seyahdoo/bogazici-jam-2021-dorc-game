b23e320da84f709c3ee4c7dc88dc1976b59e06a5
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-05 15:06:53 +0300

    feat character look to mouse

---
+++ Assets/Game/Character/CharacterLookMouse.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLookMouse : MonoBehaviour {
+    private Camera _camera;
+    private void Awake() {
+        _camera = Camera.main;
+    }
+    private void Update() {
+        var position = transform.position;
+        var plane = new Plane(Vector3.up, position);
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float enter)) {
+            var point = ray.GetPoint(enter);
+            var direction = point - position;
+            var angle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+            transform.eulerAngles = new Vector3(0f, angle, 0f);
+        }
+    }
+}
