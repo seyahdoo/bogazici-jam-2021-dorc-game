7eef35252cb02ed4985469d05f0b7905fd86385e
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-06-05 15:39:14 +0300

    character attack walk animation

--- Assets/Game/Character/Code/CharacterAttack.cs
+++ Assets/Game/Character/Code/CharacterAttack.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class CharacterAttack : MonoBehaviour {
     public GameObject hitBox;
     public float hitActiveDuration = 1f;
 
+    private Animator _animator;
     private float _hitEndTime;
-    private bool _hittting;
+    private bool _hitting;
+    private static readonly int AttackStateHash = Animator.StringToHash("Attack");
     private void Awake() {
         hitBox.SetActive(false);
-        _hittting = false;
+        _hitting = false;
         _hitEndTime = Time.time;
+        _animator = GetComponent<Animator>();
     }
     private void Update() {
         if (Input.GetButton("Fire1")) {
-            _hittting = true;
+            _hitting = true;
             _hitEndTime = Time.time + hitActiveDuration;
             hitBox.SetActive(true);
+            _animator.SetTrigger(AttackStateHash);
         }
-        if (_hittting && Time.time > _hitEndTime) {
-            _hittting = false;
+        if (_hitting && Time.time > _hitEndTime) {
+            _hitting = false;
             hitBox.SetActive(false);
         }
     }
